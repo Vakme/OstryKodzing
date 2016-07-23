ee9a59ef9924999bbd3979fbee9610f98a83c38c
Marcin Pajka <mpajka9@gmail.com>
2016-07-23 10:46:54 +0200

    login.py- update

--- login.py
+++ login.py
@@ -1,6 +1,8 @@
 
+from Strategia import *
 
-class login:
+
+class login(Strategia):
     def __init__(self,nowyWidok = 0, nowyModel = 0):
         self.widok=nowyWidok
         self.model=nowyModel
@@ -11,3 +13,11 @@ class login:
             print "Witaj " + self.widok.nick + "!"
         else:
             print "Zly nick i/lub haslo!"
+
+
+    def pobierzDane(self, idZadania):  # Uzywa metod Modelu do pobrania danych z bazy, pobiera dane ze zmiennych Modelu
+        pass
+    def wyslijDane(self,idZadania):  # Wysyla dane do zmiennych Modelu, aktualizuje baze wywolujac odpowiednia funkcje Modelu
+        pass
+    def main(self, args=None):  # Glowna funkcja podprogramu
+        pass
